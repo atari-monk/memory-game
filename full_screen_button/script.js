65af5b6111f1cad96c7e5e137da3e11a0d402f25
atari-monk <atari.monk1@gmail.com>
2025-01-01 13:47:58 +0100

    feat(full screen button): prototype of component

--- full_screen_button/script.js
+++ full_screen_button/script.js
@@ -1,2 +1,38 @@
-// Add your game logic here
-console.log('Game initialized');
+class FullscreenToggle {
+  constructor(buttonId, elementId) {
+    this.fullscreenButton = document.getElementById(buttonId)
+    this.elementId = elementId
+    this.init()
+  }
+
+  init() {
+    if (this.fullscreenButton) {
+      this.fullscreenButton.addEventListener('click', () => {
+        this.toggleFullscreen()
+        this.fullscreenButton.style.display = 'none'
+      })
+
+      document.addEventListener('fullscreenchange', () => {
+        if (!document.fullscreenElement) {
+          this.fullscreenButton.style.display = 'block'
+        }
+      })
+    }
+  }
+
+  toggleFullscreen() {
+    const element = this.elementId
+      ? document.getElementById(this.elementId)
+      : document.documentElement
+    if (!document.fullscreenElement) {
+      element.requestFullscreen().catch((err) => {
+        console.error(`Error attempting to enable fullscreen: ${err.message}`)
+      })
+    } else {
+      document.exitFullscreen().catch((err) => {
+        console.error(`Error attempting to exit fullscreen: ${err.message}`)
+      })
+    }
+  }
+}
+new FullscreenToggle('fullscreenButton')
